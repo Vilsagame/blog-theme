0a493df059c1b92f10610ff9d1078bfa90f8db6d
Cyril POIDEVIN <vilsafur@gmail.com>
2017-01-04 17:03:01 +0100

    Added basic header and footer

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <!-- <title><?php echo wp_title(); ?></title> -->
+  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0">
+  <meta name="description" content="a changer">
+  <?php wp_head(); ?>
+</head>
+<body>
