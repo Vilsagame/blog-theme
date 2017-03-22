f95a7aab79d2ad5efc2b7e6495bf07d16ba95791
Cyril POIDEVIN <vilsafur@gmail.com>
2017-03-22 16:52:47 +0100

    Bugfix widget placement in footer

--- footer.php
+++ footer.php
@@ -14,17 +14,35 @@
       ?>
       <?php
       if(is_active_sidebar('footer_widget_second')) :
+        ?>
+        <div id="second" class="widget-area">
+        <?php
         dynamic_sidebar('footer_widget_second');
+          ?>
+        </div>
+          <?php
       endif;
       ?>
       <?php
       if(is_active_sidebar('footer_widget_third')) :
+        ?>
+        <div id="third" class="widget-area">
+        <?php
         dynamic_sidebar('footer_widget_third');
+          ?>
+        </div>
+          <?php
       endif;
       ?>
       <?php
       if(is_active_sidebar('footer_widget_fourth')) :
+        ?>
+        <div id="fourth" class="widget-area">
+        <?php
         dynamic_sidebar('footer_widget_fourth');
+          ?>
+        </div>
+          <?php
       endif;
       ?>
   </div>
