1351796f33fa7f01299498276d2dae8d96325a35
Cyril POIDEVIN <vilsafur@gmail.com>
2019-09-28 13:19:44 +0200

    fix: update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,129 @@
-/////////////////////////
-/// /file Gulpfile.js
-/// /version 0.1
-/// /author Cyril POIDEVIN
-///
-/// Gulpfile's definition
-///
-/////////////////////////
+/**
+ * @file Gulpfile.js
+ * @version 0.1
+ * @author Cyril POIDEVIN
+ *
+ * Gulpfile's definition
+ */
 
-// Plugins
-// -------
+/**
+ * Global Plugins
+ */
+const { src, dest, series, parallel, watch } = require('gulp')
+const imagemin = require('gulp-imagemin')
+const gulpUseref = require('gulp-useref')
+const plumber = require('gulp-plumber')
+const sourcemaps = require('gulp-sourcemaps')
+const rename = require('gulp-rename')
+const eslint = require('gulp-eslint')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+const sass = require('gulp-sass')
+const csso = require('gulp-csso')
+const cssbeautify = require('gulp-cssbeautify')
+const csscomb = require('gulp-csscomb')
+const autoprefixer = require('gulp-autoprefixer')
 
-// Global Plugins
-var gulp = require("gulp")
-var useref = require('gulp-useref')
-var runSequence = require('run-sequence')
-var requireDir = require('require-dir')
+/**
+ * Development Tasks
+ */
 
-requireDir('./gulpTasks')
+ /**
+  * Format JS
+  */
+function scripts () {
+  return src('./assets/js/app.js')
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(dest('./assets/js/'))
+}
 
-// Development Tasks
-// -----------------
+/**
+ * Convert Sass to CSS
+ */
+function styles () {
+  return src('./assets/sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(csscomb())
+    .pipe(cssbeautify({indent: '  ', autosemicolon: true}))
+    .pipe(autoprefixer())
+    .pipe(dest('./assets/css/'))
+}
 
-// Watchers
-gulp.task('watch', function(){
-  gulp.watch("./assets/sass/**/*.scss", ["styles"]);
-  gulp.watch("./assets/js/**/*.js", ["scripts"]);
-})
+/**
+ * Watchers
+ */
+function watcher () {
+  watch('./assets/sass/**/*.scss', { delay: 500 }, styles)
+  watch('./assets/js/**/*.js', { delay: 500 }, scripts)
+}
 
-// Optimization Tasks
-// ------------------
+/**
+ * Optimization Tasks
+ */
 
-// Concat js and css
-gulp.task('useref', function(){
-  return gulp.src('./*.html')
-    .pipe(useref())
-    .pipe(gulp.dest(dist))
-})
+/**
+ * Minify JS
+ */
+function minifyScripts () {
+  return src(['./assets/js/*.js', '!./assets/js/*.min.js'])
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(dest('./assets/js/'))
+}
 
-// Build Sequences
-// ---------------
+/**
+ * Minify Styles
+ */
+function minifyStyles () {
+  return src(['./assets/css/*.css', '!./assets/css/*.min.css'])
+    .pipe(sourcemaps.init())
+    .pipe(csso())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(dest('./assets/css/'))
+}
+
+/**
+ * Minify fonts
+ */
+function fonts () {
+  return src('./assets/fonts/**/*')
+    .pipe(imagemin())
+    .pipe(dest('./assets/fonts/'))
+}
+
+/**
+ * Minify images
+ */
+function picts () {
+  return src('./assets/picts/**/*.+(png|jpg|gif|svg)')
+    .pipe(imagemin())
+    .pipe(dest('./assets/picts/'))
+}
+
+/**
+ * Concat js and css
+ */
+function useref () {
+  return src('./*.html')
+    .pipe(gulpUseref())
+    .pipe(dest('dist'))
+}
 
 // Default Task
-gulp.task("default", function(callback) {
-  runSequence(
-    ["styles", "scripts"],
-    ["watch"],
-    callback
-  )
-})
+exports.default = series(parallel(styles, scripts), watcher)
 
 // Build Task
-gulp.task("build", function(callback) {
-  runSequence(
-    ['minify:styles', 'minify:scripts', 'picts', 'fonts', 'useref'],
-    callback
-    )
-})
+exports.build = parallel(minifyStyles, minifyScripts, picts, fonts, useref)
